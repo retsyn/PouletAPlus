ee54163e84303ba6731ece68d9742df381a86998
Matthew Riche <matthew.riche@gmail.com>
2024-02-13 13:24:42 -0500

    Building map class and meta-tile adding.

--- tilemap.h
+++ tilemap.h
@@ -2,10 +2,4 @@
 
 #include <Arduboy2.h>
 
-extern const uint8_t PROGMEM meta_tile01[];
-extern const uint8_t PROGMEM meta_tile02[];
-extern const uint8_t PROGMEM meta_tile03[];
-extern const uint8_t PROGMEM meta_tile04[];
-extern const uint8_t PROGMEM meta_tile05[];
-extern const uint8_t PROGMEM meta_tile06[];
-
+extern const uint8_t PROGMEM meta_tiles[];
